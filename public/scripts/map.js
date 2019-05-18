f47e4363b3f630e227f77b53562bfc341caab67b
Zenan Chen <zchen62@sheffield.ac.uk>
2019-05-18 14:44:13 +0100

    map modal

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,3 +1,44 @@
+// Code goes here
+
+$(document).ready(function() {
+    var map = null;
+    var myMarker;
+    var myLatlng;
+
+    function initializeGMap(lat, lng) {
+        myLatlng = new google.maps.LatLng(lat, lng);
+
+        var myOptions = {
+            zoom: 12,
+            zoomControl: true,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+        myMarker = new google.maps.Marker({
+            position: myLatlng
+        });
+        myMarker.setMap(map);
+    }
+
+    // Re-init map before show modal
+    $('#exampleModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        initializeGMap(button.data('lat'), button.data('lng'));
+        $("#location-map").css("width", "100%");
+        $("#map_canvas").css("width", "100%");
+    });
+
+    // Trigger map resize event after modal shown
+    $('#exampleModal').on('shown.bs.modal', function() {
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(myLatlng);
+    });
+});
+
+
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
