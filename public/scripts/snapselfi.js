b0b8f146215cfd61a68e0fafa7f32c74f5fa6fce
Mingyan <mzeng4@sheffield.ac.uk>
2019-05-19 19:34:35 +0100

    self, and navbar

--- public/scripts/snapselfi.js
+++ public/scripts/snapselfi.js
@@ -1,4 +1,9 @@
-
+/**
+ * initialize mediaDevices
+ * get elements of selectors that will be used
+ * display stream on canvas
+ * display feezed image on canvas if click snap
+ */
 function snapselfi(){
     //!! converts a value to a boolean and ensures a boolean type.
     var hasGetUserMedia = !!(navigator.mediaDevices.getUserMedia ||
@@ -16,19 +21,50 @@ function snapselfi(){
 
     var video_element = document.getElementById('player');
     var canvas = document.getElementById('snapshot');
-    var snap = document.getElementById('snap')
+    var snap = document.getElementById('snap');
     var context = canvas.getContext('2d');
     if(hasGetUserMedia){
         navigator.mediaDevices.getUserMedia({video:true})
             .then(handleSuccess)
             .catch(errocb);
-
+        //draw image to canvas when click
         snap.addEventListener("click",function(){
-            context.drawImage(video_element,0,0,640,480);
+            context.drawImage(video_element,0,0,320,240);
         });
     }else{
         console.log('someerror');
     }
 
 }
+
+
+/**
+ * Returns an Image object and download to local disk
+ * the downloaded image name is reference to the time it downloaded
+ * image attribute always be .png
+ */
+function dloadselfi(){
+        //获取canvas标签里的图片内容
+    var filename = (new Date()).getTime() + '.' + 'png';
+    var canvas = document.getElementById('snapshot');
+    download(canvas.toDataURL('image/png'),filename)
+
+}
+
+/**
+ * Returns an Image object that can then be painted can be downloaded
+ * The url argument must specify an absolute {@link URL}. The name
+ * argument is a specifier that is relative to the url argument.
+ * @param url an absolute URL giving the base location of the image
+ * @param name the location of the image, relative to the url argument
+ */
+function download(url, name) {
+    const aLink = document.createElement('a');
+    aLink.download = name;
+    aLink.href = url;
+    aLink.dispatchEvent(new MouseEvent('click', {}))
+    window.location.href = '/'
+}
+
+
 
